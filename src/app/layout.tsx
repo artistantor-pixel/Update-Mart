e33f80307be7f2857cfc39876bc60039cf9c0a24
charukulweb-hash <charukul.web@gmail.com>
2026-09-15 13:08:22 +0600

    Add Facebook Pixel tracking integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from '@/components/Providers'
+import FacebookPixel from '@/components/FacebookPixel'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -31,6 +32,7 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className} suppressHydrationWarning>
+        <FacebookPixel />
         <Providers>
           {children}
         </Providers>
---
+++ src/components/FacebookPixel.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import Script from 'next/script';
+import { useEffect, useState } from 'react';
+import * as fpixel from '@/lib/fpixel';
+
+export default function FacebookPixel() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!loaded) return;
+    
+    // Track pageview on route change
+    fpixel.pageview();
+  }, [pathname, searchParams, loaded]);
+
+  if (!fpixel.FB_PIXEL_ID) return null;
+
+  return (
+    <>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        onLoad={() => setLoaded(true)}
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${fpixel.FB_PIXEL_ID}');
+          `,
+        }}
+      />
+    </>
+  );
+}
