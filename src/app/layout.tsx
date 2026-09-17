7ce4da3cf15a3ea42daf5b4e7bf02cc53ffdfbe2
charukulweb-hash <charukul.web@gmail.com>
2026-09-17 11:19:56 +0600

    chore: add Facebook Pixel snippet to layout head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from '@/components/Providers'
-import FacebookPixel from '@/components/FacebookPixel'
-import { Suspense } from 'react'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -23,6 +21,31 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{
             __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1445895464095441');
+fbq('track', 'PageView');
+            `
+          }}
+        />
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1445895464095441&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
               if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
                 document.documentElement.classList.add('dark');
               } else {
@@ -33,9 +56,6 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className} suppressHydrationWarning>
-        <Suspense fallback={null}>
-          <FacebookPixel />
-        </Suspense>
         <Providers>
           {children}
         </Providers>
